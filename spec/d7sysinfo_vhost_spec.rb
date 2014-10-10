bb4cb70f14276a18e683ed7f747826ec03be0d4d
Andrew Hawthorne <andrew@domain7.com>
2014-10-10 09:22:24 -0700

    Add test for new Drupal version detect

--- spec/d7sysinfo_vhost_spec.rb
+++ spec/d7sysinfo_vhost_spec.rb
@@ -4,4 +4,5 @@ describe D7sysinfo::Vhost do
   it { expect(subject['wordpress.conf'][:wordpress]).to eq '7.7.7' }
   it { expect(subject['rails.conf'][:rails]).to eq 'rails (3.3.3)' }
   it { expect(subject['drupal.conf'][:drupal]).to eq '1.1.1' }
+  it { expect(subject['drupal2.conf'][:drupal]).to eq '7.26' }
 end
